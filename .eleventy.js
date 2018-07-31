806bde84703612ce95cc738a11d92466dd257a5d
Eric Portis <e@ericportis.com>
2018-07-31 10:27:24 -0700

    cldimg → respimg, fallbackWidth, spaces between srcsets, comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
 module.exports = function( eleventyConfig ) {
 
+	// change me!
 	eleventyConfig.cloudinaryCloudName = 'eric-cloudinary';
 	eleventyConfig.srcsetWidths = [ 320, 640, 960, 1280, 1600, 1920, 2240, 2560 ];
+	eleventyConfig.fallbackWidth = 640;
 
-	eleventyConfig.addShortcode( 'cldimg', function( src, alt, sizes ) {
+	// {% respimg %} shortcode
+	eleventyConfig.addShortcode( 'respimg', function( src, alt, sizes ) {
 		
 		const fetchBase = `https://res.cloudinary.com/${ eleventyConfig.cloudinaryCloudName }/image/fetch/`;
 		
@@ -12,10 +15,10 @@ module.exports = function( eleventyConfig ) {
 				srcset="${
 					eleventyConfig.srcsetWidths.map( ( w ) => {
 						return `${ fetchBase }q_auto,f_auto,w_${ w }/${ src } ${ w }w`
-					} ).join( ',' )
+					} ).join( ', ' )
 				}"
 				sizes="${ sizes ? sizes : '100vw' }"
-				src="${ fetchBase }q_auto,f_auto/${ src }"
+				src="${ fetchBase }q_auto,f_auto,w_${ eleventyConfig.fallbackWidth }/${ src }"
 				${ alt ? `alt="${ alt }"` : '' }
 			/>
 		`;
