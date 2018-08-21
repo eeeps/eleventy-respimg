3856464ab49e97af992a0b5088995fbc85dc7b3a
Eric Portis <e@ericportis.com>
2018-08-20 22:38:37 -0700

    Got rid of extra whitespace (problematic in markdown files). (Also, added a processed markdown file in order to test this) Fixes #2

--- .eleventy.js
+++ .eleventy.js
@@ -10,19 +10,22 @@ module.exports = function( eleventyConfig ) {
 		
 		const fetchBase = `https://res.cloudinary.com/${ eleventyConfig.cloudinaryCloudName }/image/fetch/`;
 		
-		return `
-			<img
-				srcset="${
-					eleventyConfig.srcsetWidths.map( ( w ) => {
-						return `${ fetchBase }q_auto,f_auto,w_${ w }/${ src } ${ w }w`
-					} ).join( ', ' )
-				}"
-				sizes="${ sizes ? sizes : '100vw' }"
-				src="${ fetchBase }q_auto,f_auto,w_${ eleventyConfig.fallbackWidth }/${ src }"
-				${ alt ? `alt="${ alt }"` : '' }
-			/>
-		`;
+		return `<img
+	srcset="${
+		eleventyConfig.srcsetWidths.map( ( w ) => {
+			return `${ fetchBase }q_auto,f_auto,w_${ w }/${ src } ${ w }w`
+		} ).join( ', ' )
+	}"
+	sizes="${ sizes ? sizes : '100vw' }"
+	src="${ fetchBase }q_auto,f_auto,w_${ eleventyConfig.fallbackWidth }/${ src }"
+	${ alt ? `alt="${ alt }"` : '' }
+/>`;
 	
 	} );
+
+	// other config options
+	return {
+		markdownTemplateEngine: "njk"
+	}
 	
 };
---
+++ markdown.md
@@ -0,0 +1,11 @@
+---
+layout: layout.njk
+---
+
+Well hello there.
+
+{% respimg
+	"https://ericportis.com/about/transparent-potatoes-full.png",
+	"Me, as Mister Potato Head",
+	"(min-width: 48em) 48em, 100vw"
+%}
